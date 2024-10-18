91ed3993029a9f9db7081428e4aa16b7ecb9e44c
Lucky Mbithi <luckymbithi002@gmail.com>
2024-10-18 20:19:30 +0300

    add user controller APIs

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
+// packages
+import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
-import connectMongoDB from "./db/connectMognoDb.js";
-import authRoutes from "./routes/auth-routes.js";
 
+// routes
+import authRoutes from "./routes/auth-routes.js";
+import userRoutes from "./routes/user-routes.js";
+
+// utility func
+import connectMongoDB from "./db/connectMognoDb.js";
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 const app = express();
 
 app.use(express.json());
@@ -12,6 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.json({
