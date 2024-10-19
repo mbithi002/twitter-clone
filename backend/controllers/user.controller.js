e83fca554d91fa9d3b7d30123ecd790fbef48b27
Lucky Mbithi <luckymbithi002@gmail.com>
2024-10-19 15:31:07 +0300

    ---

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,17 +6,15 @@ import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
+
   try {
     const user = await User.findOne({ username }).select("-password");
-    if (user) {
-      return res.status(404).json({
-        message: "user not found",
-      });
-    }
+    if (!user) return res.status(404).json({ message: "User not found" });
+
     res.status(200).json(user);
   } catch (error) {
+    console.log("Error in getUserProfile: ", error.message);
     res.status(500).json({ error: error.message });
-    console.log("Error in getUserProfile", error);
   }
 };
 
