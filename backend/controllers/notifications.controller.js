ff96e6aa2ded606e24aa489f38d8b7304357b6aa
Lucky Mbithi <luckymbithi002@gmail.com>
2024-10-21 10:05:59 +0300

    add notifications func

--- backend/controllers/notifications.controller.js
+++ backend/controllers/notifications.controller.js
@@ -10,7 +10,7 @@ export const getNotifications = async (req, res) => {
     });
     await Notification.updateMany({ to: userId }, { read: true });
 
-    res.status(200).json({ notifications });
+    res.status(200).json(notifications);
   } catch (error) {
     console.log("Error in getNotifications: ", error.message);
     res.status(500).json({ message: "Internal server error" });
